d449eea6a37d50af56efb61aecc4f3e9428dce31
Marcus Vinicius <58367557+mad-lnx@users.noreply.github.com>
2020-05-17 23:05:35 -0300

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 import scapy.all as scapy
 
@@ -7,13 +6,16 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print(answered_list.summary())
 
     print("#######################################################")
     print("IP\t\t\tMAC Address")
     print("#######################################################")
+    clients_list = []
     for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
         print("#######################################################")
+    print(clients_list)
 
 scan("192.168.1.1/24")
